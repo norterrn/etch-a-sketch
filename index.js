2a66e969f99394f1da0228a71d5cd8692d6ad5ae
norterrn <andrea.pasquariello2002@gmail.com>
2022-10-12 14:05:06 +0200

    Fixed Size buttons, colors when drawing, added eraser (color to white) and reset (generates new 16x16 grid)

--- index.js
+++ index.js
@@ -4,42 +4,71 @@ const medium = document.querySelector(".medium");
 const large = document.querySelector(".large");
 const classic = document.querySelector(".classic");
 const rainbow = document.querySelector(".rainbow");
+const eraser = document.querySelector(".eraser");
 const clear = document.querySelector(".clear");
-const initialColor = "white";
 let currentColor = "black";
 
+
 function generateGrid(size) {
-  // ---------- created the divs
   draw.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   draw.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement("div");
     draw.insertAdjacentElement("beforeend", square);
-    draw.addEventListener("mousedown", function () {
-      square.className = `${currentColor}`;
-    });
-
-    square.addEventListener("contextmenu", function () {
-      window.addEventListener("contextmenu", (e) => e.preventDefault());
-    });
+    square.addEventListener("mouseover", changeColor);
+    square.addEventListener("mousedown", changeColor);
+  }
+}
+// ----- sets the let mouseDown to false
+// --- when mouse is clicked by user, changes value to true
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+// ------------------- when that value is true mouseOver works
+function changeColor(event) {
+  if (event.type === "mouseover" && !mouseDown) return;
+  if (currentColor === "black") {
+    event.target.style.backgroundColor = `${currentColor}`;
+  } else if (currentColor === "white") {
+    event.target.style.backgroundColor = "white";
+  } else if (currentColor != "black") {
+    event.target.style.backgroundColor = `${randomColor()}`;
   }
 }
 
-generateGrid(8);
+function randomColor() {
+  let random = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  return random;
+}
 
-small.addEventListener("click", function () {
+small.addEventListener("click", () => {
   draw.innerHTML = "";
+  currentColor = "black";
   generateGrid(8);
 });
-medium.addEventListener("click", function () {
+medium.addEventListener("click", () => {
+  draw.innerHTML = "";
+  currentColor = "black";
+  generateGrid(16);
+});
+large.addEventListener("click", () => {
+  draw.innerHTML = "";
+  currentColor = "black";
+  generateGrid(48);
+});
+classic.addEventListener("click", () => {
+  currentColor = "black";
+});
+rainbow.addEventListener("click", () => {
+  randomColor();
+  currentColor = `${randomColor()}`;
+});
+eraser.addEventListener("click", () => {
+  currentColor = "white";
+});
+clear.addEventListener("click", () => {
   draw.innerHTML = "";
   generateGrid(16);
 });
-large.addEventListener("click", function () {
-  draw.innerHTML = "";
-  generateGrid(48);
-});
-classic.addEventListener("click", function () {
-  currentColor = "black";
-});
+
+generateGrid(16);
